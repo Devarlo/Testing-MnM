fee17ae6e192593207999bc9b9229f2c03a980e0
Devarlo <devarlorazan05@gmail.com>
2026-03-23 22:08:36 +0700

    ngubah migration MnM + routes

--- Modules/ManajemenMahasiswa/routes/web.php
+++ Modules/ManajemenMahasiswa/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\ManajemenMahasiswa\Http\Controllers\ManajemenMahasiswaController;
-
-Route::middleware('auth')->group(function () {
-    Route::get('/mahasiswa/dashboard', [ManajemenMahasiswaController::class, 'dashboard'])
-        ->name('mahasiswa.dashboard');
-});
+use Modules\ManajemenMahasiswa\Http\Controllers\MkDashboardController;
+use Modules\ManajemenMahasiswa\Http\Controllers\KemahasiswaanController;
+use Modules\ManajemenMahasiswa\Http\Controllers\AlumniController;
+use Modules\ManajemenMahasiswa\Http\Controllers\PengumumanController;
+use Modules\ManajemenMahasiswa\Http\Controllers\KegiatanController;
+use Modules\ManajemenMahasiswa\Http\Controllers\ForumController;
+use Modules\ManajemenMahasiswa\Http\Controllers\PengurusHimaskomController;
+use Modules\ManajemenMahasiswa\Http\Controllers\PengaduanController;
+use Modules\ManajemenMahasiswa\Http\Controllers\GamificationController;
+use Modules\ManajemenMahasiswa\Http\Controllers\CvController;
